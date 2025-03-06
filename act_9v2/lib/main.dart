fb223deef3d11c13d074cc52363e9b711ce111c3
Gregory-Vaughan <gpv520@gmail.com>
2025-03-05 20:29:23 -0500

    5th Commit, did card screen -G

--- act_9v2/lib/main.dart
+++ act_9v2/lib/main.dart
@@ -40,6 +40,13 @@ class FoldersScreen extends StatelessWidget {
 
 class CardsScreen extends StatelessWidget {
   final String folderName;
+  final List<Map<String, dynamic>> cards = [
+    {'name': 'Ace', 'image': 'assets/Ace.png'},
+    {'name': 'King', 'image': 'assets/King.png'},
+    {'name': 'Queen', 'image': 'assets/Queen.png'},
+    {'name': 'Jack', 'image': 'assets/Jack.png'},
+    {'name': '10', 'image': 'assets/10.png'},
+  ];
 
   CardsScreen({required this.folderName});
 
@@ -47,17 +54,33 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('$folderName Cards')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Folders'),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemCount: cards.length,
+        itemBuilder: (context, index) {
+          final card = cards[index];
+          return Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(card['image'], width: 80, height: 80),
+                SizedBox(height: 10),
+                Text(card['name'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
+
 
 void main() {
   runApp(MaterialApp(
