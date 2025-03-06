2555c41b48ec80bcb7930109a892fc32e142e3b5
Gregory-Vaughan <gpv520@gmail.com>
2025-03-05 19:53:43 -0500

    3rd Commit, in the middle of changing images -G

--- act_9v2/lib/main.dart
+++ act_9v2/lib/main.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 
 
 class FoldersScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> folders = [
+    {'name': 'Hearts', 'image': 'assets/Hearts.png', 'count': 3},
+    {'name': 'Spades', 'image': 'assets/Spades.png', 'count': 5},
+    {'name': 'Diamonds', 'image': 'assets/Diamonds.png', 'count': 4},
+    {'name': 'Clubs', 'image': 'assets/Clubs.png', 'count': 6},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Folders')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => CardsScreen()),
-            );
-          },
-          child: Text('Go to Cards'),
-        ),
+      body: ListView.builder(
+        itemCount: folders.length,
+        itemBuilder: (context, index) {
+          final folder = folders[index];
+          return ListTile(
+            leading: Image.asset(folder['image'], width: 50, height: 50),
+            title: Text(folder['name']),
+            subtitle: Text('${folder['count']} cards'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CardsScreen(folderName: folder['name']),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
@@ -23,10 +39,14 @@ class FoldersScreen extends StatelessWidget {
 
 
 class CardsScreen extends StatelessWidget {
+  final String folderName;
+
+  CardsScreen({required this.folderName});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Cards')),
+      appBar: AppBar(title: Text('$folderName Cards')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
